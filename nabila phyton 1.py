67fe8692f005db3b3272401110c5fe3975a9df17
nabilatabaika07 <163220432+nabilatabaika07@users.noreply.github.com>
2024-03-13 10:39:45 +0900

    Add files via upload

---
+++ nabila phyton 1.py
@@ -0,0 +1,9 @@
+data_tuple = ('John','david','bryan')
+
+data_list = list(data_tuple)
+
+data_list[0] = 'elon'  # Mengubah nama 'John' menjadi 'elon
+data_tuple_modified = tuple(data_list)
+
+print(data_tuple_modified)
+
